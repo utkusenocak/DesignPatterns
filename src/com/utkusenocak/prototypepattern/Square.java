e2bd3ec68a6e50b52af4622b7fbd1a47dd7f28a1
Utku SENOCAK <u.senocakk@gmail.com>
2021-12-30 23:02:06 +0300

    Add Prototype Design Pattern

---
+++ src/com/utkusenocak/prototypepattern/Square.java
@@ -0,0 +1,13 @@
+package com.utkusenocak.prototypepattern;
+
+public class Square extends Shape {
+
+    public Square() {
+        type = "Square";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
