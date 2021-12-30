307004759b8468d50675d7be0c3f3bc8df764ca9
Utku SENOCAK <u.senocakk@gmail.com>
2021-12-30 23:45:25 +0300

    Add Composite Design Pattern

---
+++ src/com/utkusenocak/compositepattern/Employee.java
@@ -0,0 +1,34 @@
+package com.utkusenocak.compositepattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private final String name;
+    private final String dept;
+    private final int salary;
+    private final List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        subordinates = new ArrayList<>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public String toString() {
+        return ("Employee: [ Name: " + name + ", dept: " + dept + ", salary: " + salary + " ]");
+    }
+}
